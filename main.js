3f8b2d566d5283504a8d3524b6801565f135eeaa
Rushaun6 <166856723+Rushaun6@users.noreply.github.com>
2026-07-16 04:46:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log('rpg starter');
+const config = {
+    type: Phaser.AUTO,
+
+    parent: "game",
+
+    width: 1280,
+    height: 720,
+
+    backgroundColor: "#202020",
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+
+    scene: {
+
+        create() {
+
+            this.add.text(
+                460,
+                340,
+                "rpg project",
+                {
+                    fontSize: "32px",
+                    color: "#ffffff"
+                }
+            );
+
+        }
+
+    }
+
+};
+
+new Phaser.Game(config);
